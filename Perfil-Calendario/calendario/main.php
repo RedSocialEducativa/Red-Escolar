3bb0f646fbeac19a0ea180ecc718dfd5a4932834
Diana Coello <diana_scs1994@hotmail.com>
2014-06-26 09:10:11 -0500

    Calendario y Perfil

---
+++ Perfil-Calendario/calendario/main.php
@@ -0,0 +1,100 @@
+<?php
+echo <<< HERE
+
+<div class="content">
+
+  
+  <section>
+   
+  <div class="bloque1">
+    Planificador<br>
+        <div class="bs-docs-section">
+             <table class="table table-striped table-hover ">
+                <thead>
+                  <tr>
+               
+                    <th>Dom</th>
+                    <th>Lun</th>
+                    <th>Mar</th>
+             <th>Mier</th>
+                    <th>Jue</th>
+                    <th>Vier</th>
+             <th>Sab</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                   <th>1</th>
+        <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+                  </tr>
+                  <tr>
+                     <th>1</th>
+        <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+                  </tr>
+                  <tr class="info">
+                    <th>1</th>
+        <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+                  </tr>
+                  <tr class="success">
+                     <th>1</th>
+        <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+                  </tr>
+                  <tr class="danger">
+                    <th>1</th>
+        <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+                  </tr>
+                  <tr class="warning">
+                    <th>1</th>
+        <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+     <th>1</th>
+                  </tr>
+                </tbody>
+              </table> 
+            </div><!-- /example -->
+        
+          </div>
+  </section>
+
+
+  <section>
+    <div class="bloque1">
+
+      <p><a1>InformacionActualizada</p></a1>
+
+    </div>
+  </section>
+  </div>
+
+HERE;
+
+?> 
---
+++ Perfil-Calendario/calendario/navegacion.php
@@ -0,0 +1,34 @@
+<?php
+echo <<< HERE
+
+<nav id="navegacion">
+           <div class="navbar navbar-default">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+      <form class="navbar-form navbar-left">
+        <input type="text" class="form-control col-lg-8" placeholder="Search">
+      </form>
+  </div>
+  <div class="navbar-collapse collapse navbar-responsive-collapse">
+    <ul class="nav navbar-nav">
+	<li class="active"><a class="navbar-brand" href="#">Inicio</a></li>
+      <li><a href="#"><img src="images/noticias.png"/></a></li>
+    </ul>
+    
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><img src="images/perfil.png"/></a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Configuraciones<b class="caret"></b></a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>  
+
+HERE;
+
+?> 
