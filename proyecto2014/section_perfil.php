66ee39b815ff1bb080806a466c67d314d54f3726
DianaCoello <diana_scs1994@hotmail.com>
2014-07-14 15:58:19 -0500

    Cambio

--- proyecto2014/section_perfil.php
+++ proyecto2014/section_perfil.php
@@ -26,7 +26,7 @@
 		</article>
 		<article>
 			<div class="heading">
-				<h2><a href="index.html">Grupo Matematica</a></h2>
+				<h2><a href="index.php">Grupo Matematica</a></h2>
 				<div class="info">>>> Posted by Admin | Agosto 23, 2014</div>
 			</div>
 			<div class="content">
@@ -38,7 +38,7 @@
 		</article>
 		<article>
 			<div class="heading">
-				<h2><a href="index.html">Estefano Garcia</a></h2>
+				<h2><a href="index.php">Estefano Garcia</a></h2>
 				<div class="info">>>> Posted by Admin | Agosto 11, 2014</div>
 			</div>
 			<div class="content">
@@ -50,7 +50,7 @@
 		</article>
 		<article>
 			<div class="heading">
-				<h2><a href="index.html">MAteo Mejia</a></h2>
+				<h2><a href="index.php">Mateo Mejia</a></h2>
 				<div class="info">>>> Posted by Admin | Agosto 01, 2014 </div>
 			</div>
 			<div class="content">
@@ -77,7 +77,7 @@
 			<div class="content">
 				<div class="post">
 					<img src="images/thumb2.jpg"/>
-					<h4><a href="single.html">Link de descarga de material didactico</a></h4>
+					<h4><a href="single.php">Link de descarga de material didactico</a></h4>
 					<p>Mayo 21 ,2014</p>
 				</div>
 				<div class="post">
@@ -85,7 +85,7 @@
 				</div>
 				<div class="post">
 					<img src="images/thumb1.jpg"/>
-					<h4><a href="single.html">Ayudas a domicilio de matematica</a></h4>
+					<h4><a href="single.php">Ayudas a domicilio de matematica</a></h4>
 					<p>Junio 11 ,2014</p>
 				</div>
 					<div class="post">
@@ -94,7 +94,7 @@
 				
 				<div class="post">
 					<img src="images/thumb3.jpg"/>
-					<h4><a href="single.html">Entradas al evento de desarrollo social a la comunidad</a></h4>
+					<h4><a href="single.php">Entradas al evento de desarrollo social a la comunidad</a></h4>
 					<p>November 21 ,2013</p>
 				</div>
 				<div class="post">
@@ -102,7 +102,7 @@
 				</div>
 				<div class="post">
 					<img src="images/thumb1.jpg"/>
-					<h4><a href="single.html">Clases de recuperacion de fisica</a></h4>
+					<h4><a href="single.php">Clases de recuperacion de fisica</a></h4>
 					<p>Junio 13 ,2013</p>
 				</div>
 				<div class="post">
@@ -110,7 +110,7 @@
 				</div>
 				<div class="post">
 					<img src="images/thumb2.jpg"/>
-					<h4><a href="single.html">Venta de libro para ayuda benefica</a></h4>
+					<h4><a href="single.php">Venta de libro para ayuda benefica</a></h4>
 					<p>Mayo 11 ,2013</p>
 				</div>
 			</div>
@@ -128,10 +128,10 @@
 		<div class="preview">	
 			<div class="letras"><h2><img src="images/chat1.png"/>Chat</h2></div>
 				<ul id="pagi">
-					<li><a href="perfil.html"><img src="images/persona.png"/>Amigo L</a></li>
-					<li><a href="perfil.html"><img src="images/persona.png"/>Mateo</a></li>
-					<li><a href="perfil.html"><img src="images/persona.png"/>Priscila</a></li>
-					<li><a href="perfil.html"><img src="images/persona.png"/>Enrique</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Amigo L</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Mateo</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Priscila</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Enrique</a></li>
 				</ul>
 			</div>
 		</section>
