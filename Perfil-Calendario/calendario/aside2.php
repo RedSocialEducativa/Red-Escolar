e7f82300e9be49a2cd9107084dd7869614810b45
mariuxifernandez <mariuxi.de.segarra@gmail.com>
2014-07-13 23:04:17 -0500

    Revert "borrado calendario"
    This reverts commit c5ccfdeb36428ac40f298ebf2263957e5e2e251c.

---
+++ Perfil-Calendario/calendario/aside2.php
@@ -0,0 +1,17 @@
+<?php
+echo <<< HERE
+
+ <aside class="col-sm-4">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">
+              GRUPOS
+            </a>
+            <a href="#" class="list-group-item">Grupo 1</a>
+            <a href="#" class="list-group-item">Grupo 2</a>
+            <a href="#" class="list-group-item">Grupo 3</a>
+            <a href="#" class="list-group-item">Grupo 4</a>
+          </aside>
+
+HERE;
+
+?> 
