fc9f5ef000990bad2e1f1a964828bf98333d3881
DianaCoello <diana_scs1994@hotmail.com>
2014-07-13 23:23:35 -0500

    php de mensajes y actividades

---
+++ section_actividades.php
@@ -0,0 +1,50 @@
+<?php
+echo <<< HERE
+
+<section id="content">
+	<div id="main-content">
+		<article>
+			<img src="PNG/user.png"/>
+			<div class="post">
+				<h4><a href="grupos.html">Grupos</a></h4>
+			</div>
+		</article>
+		<article>
+			<img src="PNG/calendar.png"/>
+			<div class="post">
+				<h4><a href="eventos.php">Eventos</a></h4>
+			</div>
+		</article>
+		<article>
+			<img src="PNG/folder.png"/>
+			<div class="post">
+				<h4><a href="archivos.php">Archivos</a></h4>
+			</div>
+			<article>
+			<img src="PNG/eye.png"/>
+			<div class="post">
+				<h4><a href="notificaciones.php">Notificaciones</a></h4>
+			</div>
+		</article>
+		</article>
+			</div>
+	<div id="sidebar">
+			<section>
+			
+	<div class="preview">	
+			<div class="letras"><h2><img src="images/chat1.png"/>Chat</h2></div>
+				<ul id="pagi">
+					<li><a href="perfil.php"><img src="images/persona.png"/>Amigo L</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Mateo</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Priscila</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Enrique</a></li>
+				</ul>
+			</div>
+		</section>
+		
+	</div>
+	<div class="clear"></div>
+</section>
+
+HERE;
+?>
---
+++ section_mensajes.php
@@ -0,0 +1,94 @@
+ <?php
+echo <<< HERE
+
+<section id="content">
+	<div id="main-content">
+		<article>
+			<div class="heading">
+				<h2>Link de Descarga</h2>
+			</div>
+			<div class="content">
+				<img src="images/port1.jpg"/>
+				<p>En la actualidad existen varias herramientas que ayuden a facilitar la informacion al estudiante, ya que en el internet una encuentra la informacion que desee en cuestion de segundos, por eso es importante tener en cuenta los sitios que nos brindan soluciones rapidas atravez de aplicaciones .</p>
+				<p>4 shared.com  nos permite obtener programas gratis de manera comprimida y muy facil de descargar y usar, sin necesidad de codigos.</p>
+					</div>
+		</article>
+		<article>
+				<div class="comment">
+		Responder
+				<div><textarea rows="1" name="comment" id="comment"></textarea></div>
+				<div><input type="submit" name="public" value="Enviar"></div>
+		</div>
+		</article>
+			</div>
+<div id="sidebar">
+	
+		<section>
+			<div class="heading"><h2>Mi Buzon</h2></div>
+			<div class="content">
+				<div class="post">
+					<img src="images/thumb2.jpg"/>
+					<h4><a href="single.php">Link de descarga de material didactico</a></h4>
+					<p>Mayo 21 ,2014</p>
+				</div>
+				<div class="post">
+					.
+				</div>
+				<div class="post">
+					<img src="images/thumb1.jpg"/>
+					<h4><a href="single.php">Ayudas a domicilio de matematica</a></h4>
+					<p>Junio 11 ,2014</p>
+				</div>
+					<div class="post">
+					.
+				</div>
+				
+				<div class="post">
+					<img src="images/thumb3.jpg"/>
+					<h4><a href="single.php">Entradas al evento de desarrollo social a la comunidad</a></h4>
+					<p>November 21 ,2013</p>
+				</div>
+				<div class="post">
+					.
+				</div>
+				<div class="post">
+					<img src="images/thumb1.jpg"/>
+					<h4><a href="single.php">Clases de recuperacion de fisica</a></h4>
+					<p>Junio 13 ,2013</p>
+				</div>
+				<div class="post">
+					.
+				</div>
+				<div class="post">
+					<img src="images/thumb2.jpg"/>
+					<h4><a href="single.php">Venta de libro para ayuda benefica</a></h4>
+					<p>Mayo 11 ,2013</p>
+				</div>
+			</div>
+		</section>
+		<section>
+			<ul id="pagi">
+				<li><a class="current" href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">next</a></li>
+			</ul>
+		</section>	<section ><h4>.</h4></section>
+		<section>
+			
+	<div class="preview">	
+			<div class="letras"><h2><img src="images/chat1.png"/>Chat</h2></div>
+				<ul id="pagi">
+					<li><a href="perfil.php"><img src="images/persona.png"/>Amigo L</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Mateo</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Priscila</a></li>
+					<li><a href="perfil.php"><img src="images/persona.png"/>Enrique</a></li>
+				</ul>
+			</div>
+		</section>
+		
+	</div>
+	<div class="clear"></div>
+</section>
+HERE;
+?>
