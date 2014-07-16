2df9dbca0acb07a0d25247b062dbb7af8d2d1abc
mariuxifernandez <mariuxi.de.segarra@gmail.com>
2014-07-16 08:36:01 -0500

    actulizar

---
+++ section_single.php
@@ -0,0 +1,43 @@
+<?php
+echo <<< HERE
+
+<section id="content">
+	<div id="main-content">
+		<article>
+			<div class="heading">
+				<h2>Link de Descarga</h2>
+			</div>
+			<div class="content">
+				<img src="images/port1.jpg"/>
+				<p>En la actualidad existen varias herramientas que ayuden a facilitar la informacion al estudiante, ya que en el internet una encuentra la informacion que desee en cuestion de segundos, por eso es importante tener en cuenta los sitios que nos brindan soluciones rapidas atravez de aplicaciones .</p>
+				<p>4 shared.com  nos permite obtener programas gratis de manera comprimida y muy facil de descargar y usar, sin necesidad de codigos.</p>
+					</div>
+		</article>
+		<article>
+				<div class="comment">
+		Mi Comentario
+				<div><textarea rows="1" name="comment" id="comment"></textarea></div>
+				<div><input type="submit" name="public" value="Publicar"></div>
+		</div>
+		</article>
+			</div>
+	<div id="sidebar">
+			<section>
+			
+	<div class="preview">	
+			<div class="letras"><h2><img src="images/chat1.png"/>Chat</h2></div>
+				<ul id="pagi">
+					<li><a href="perfil.html"><img src="images/persona.png"/>Amigo L</a></li>
+					<li><a href="perfil.html"><img src="images/persona.png"/>Mateo</a></li>
+					<li><a href="perfil.html"><img src="images/persona.png"/>Priscila</a></li>
+					<li><a href="perfil.html"><img src="images/persona.png"/>Enrique</a></li>
+				</ul>
+			</div>
+		</section>
+		
+	</div>
+	<div class="clear"></div>
+</section>
+
+HERE;
+>?
---
+++ single.php
@@ -0,0 +1,14 @@
+<html>
+<?php include('metadatos.php');?>
+<body>
+	<div class="clear"></div>
+		<?php include('header.php');?>
+	<div class="clear"></div>
+		<?php include('navegacion.php');?>
+	<div class="clear"></div>
+		<?php include('section_single.php');?>			
+	<div class="clear"></div>
+	<?php include('footer.php');?>	
+	</div>
+</body>
+</html>
