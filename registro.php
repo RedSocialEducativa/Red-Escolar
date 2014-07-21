b2a6e9c8dfc133ec11337231c69315cb0753e6f0
DianaCoello <diana_scs1994@hotmail.com>
2014-07-20 20:25:02 -0500

    Registro
    html y php de plantilla registro

---
+++ registro.php
@@ -0,0 +1,9 @@
+<html>
+<?php include('metadatos.php');?>
+<body>
+	<div class="clear"></div>
+		<?php include('formulario_registro.php');?>		
+	<div class="clear"></div>	
+	</div>
+</body>
+</html>
