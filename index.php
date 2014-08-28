23d4340d9f480869ccccad8f2f1458b8565f6fd7
mariuxifernandez <mariuxi.de.segarra@gmail.com>
2014-08-28 04:55:51 -0500

    borrados

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<?php include('metadato_keepin.php');?>
-<body>
-	<div class="clear"></div>
-		<?php include('formulario_inicio.php');?>		
-	<div class="clear"></div>	
-	</div>
-</body>
-</html>
