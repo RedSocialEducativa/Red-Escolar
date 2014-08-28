c5f4091df25dec138884f3bff6c90154c1f11c45
mariuxifernandez <mariuxi.de.segarra@gmail.com>
2014-08-28 04:57:37 -0500

    Revert "borrados"
    This reverts commit 23d4340d9f480869ccccad8f2f1458b8565f6fd7.

---
+++ eventos.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<?php include('metadatos.php');?>
+ <body role="document">
+	<div class="clear"></div>
+		<?php include('header.php');?>
+	<div class="clear"></div>
+		<?php include('navegacion.php');?>
+	<div class="clear"></div>
+		<?php include('section_evento.php');?>			
+	</div>
+</body>
+</html>
